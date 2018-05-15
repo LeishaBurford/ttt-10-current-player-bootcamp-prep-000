bd3132c58bb13362d94395a361f76d9417671b16
LeishaBurford <lburf102@uottawa.ca>
2018-05-15 23:27:19 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,10 +1,10 @@
 def turn_count(board)
   counter = 0
-  board.each |place| {
+  board.each |place| 
     if place == "X" || place == "O"
       counter += 1
     end
-  }
+  
   return counter
 end
   
